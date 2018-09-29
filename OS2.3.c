ec5e2f398a05df1f2c04a6774dbd6bb7b3653da2
RayhanSyed <rsyed528@gmail.com>
2018-09-29 18:40:47 -0400

    Update OS2.3.c
    Added error handling

--- OS2.3.c
+++ OS2.3.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]){
 		break;
 		case 2 : InitDir = argv[optind];
 		break;
-		default: perror("Invalid arguments error: Too many");
+		default: printf("Invalid arguments error: Too many");
 		exit(-1);
 		break;
 		}
@@ -31,19 +31,30 @@ int main(int argc, char *argv[]){
 		return(0);
 		}
 int DirecRead (char* D){
+	char Error[512];
 		DIR* dirp;
 		struct dirent* dp;
 		 dirp = opendir(D);
-		//fprintf(stderr, "dir %s err %s \n", D, strerror(errno));
+		 if (dirp == NULL){
+		 		snprintf(Error, sizeof(Error), "Error opening %s:", D);
+    		perror(Error);
+    	} 
 		while (dirp) {
 			char filename[257];
     	errno = 0;
     	dp = readdir(dirp);
+    	if (errno != 0 ){
+    		snprintf(Error, sizeof(Error), "Error reading %s:", D);
+    		perror(Error);
+    	}
     	if (dp != NULL) {
     		snprintf(filename, sizeof(filename), "%s/%s", D, dp->d_name);
-    	//fprintf(stderr, "filename is %s \n", dp->d_name);
     	struct stat fileStat;
     	int statSuccess= lstat(filename,&fileStat);
+    	if (errno == -1){
+    		snprintf(Error, sizeof(Error), "Error statting %s:", D);
+    		perror(Error);
+    	}
     	if(!(statSuccess == -1))  {  
     		if ((strcmp(dp->d_name, ".")==0) ||(strcmp(dp->d_name, "..")==0)){
 			FoundOutput(fileStat, filename);
@@ -55,28 +66,25 @@ int DirecRead (char* D){
     		else if(fileStat.st_mode & S_IFREG){
     	FoundOutput(fileStat, filename);
     	}
-       else if(fileStat.st_mode & S_IFREG){
-       	//fprintf(stdout, "Block: %ld BLKSIZE: %ld \n", fileStat.st_blocks, fileStat.st_blksize);	
-       //	printf("\n comp: %i \n", (S_ISLNK(fileStat.st_mode)));
-    	FoundOutput(fileStat, filename);
-    	}
     }
         }
-    
     if (dp == NULL){
-       // fprintf(stderr, "exiting \n");
         closedir(dirp);
+        if (errno == -1){
+        	snprintf(Error, sizeof(Error), "Error closing %s:", D);
+        	perror(Error);
+        }
 	return(0);
         }	
 }
 }
-
 void FoundOutput (struct stat S, char* F){
+	char Error[512];
 	struct passwd  *pwd;
 	struct group   *grp;
 	int Sym = 0;
 	printf( "%ld \t", S.st_ino);	
-	printf( "%ld \t", S.st_blocks/2);	//right number?
+	printf( "%ld \t", S.st_blocks/2);	//blocks by stat are reported in 512 byte sizes, but the find command uses 1024 bytes so there is a scaling by 1/2 to match the find command.
 	if (S_ISLNK(S.st_mode)){
 		printf("l");
 		Sym = 1;
@@ -103,11 +111,15 @@ void FoundOutput (struct stat S, char* F){
 	else
 		printf("%d \t", S.st_gid);
 	printf("%ld ",S.st_size); 	
-	printf("%s ", ctime(&S.st_mtime));
+	printf("\t  %s ", ctime(&S.st_mtime));
 	printf("%s", F);	
 	if (Sym){
 		char SymBuf[257];
-		readlink(F,SymBuf, sizeof(SymBuf)-1);
+		int rl = readlink(F,SymBuf, sizeof(SymBuf)-1);
+		if (rl == -1){
+			snprintf(Error, sizeof(Error), "Error reading contents of symlink %s:", F);
+			perror(Error);
+		}
 		printf("-> %s \n", SymBuf);
 	}
 		else
